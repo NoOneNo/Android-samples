51338b1788c297c12989e7881dd44f9c8551b5d7
zhanghaoran <zhanghaoran@xiaomi.com>
2018-06-11 16:50:05 +0800

    addacc

--- app/src/main/java/com/d/auth/AccountAuthenticator.kt
+++ app/src/main/java/com/d/auth/AccountAuthenticator.kt
@@ -1,18 +1,32 @@
 package com.d.auth
 
-import android.accounts.Account
+import android.accounts.AccountManager
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
 
 class SystemAccount {
-    fun getAccount():Account {
-        return Account("name", "type")
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun getAccounts(context: Context) {
+        val accounts = AccountManager.get(context).accounts
+
+        val accMag = AccountManager.get(context)
+        accMag.getAccountsByType("com.google")
+        accMag.getAccountsByType("com.xiaomi")
+        val intent = AccountManager.newChooseAccountIntent(null, null,
+                arrayOf("com.google", "com.xiaomi"), null, null,
+                null, null)
+
     }
 
     fun getAuthToken():String {
         return ""
     }
 
-    fun getAccountPermission() {
-//        ActivityCompat.PermissionCompatDelegate()
+    fun requestPermissions(activity: AppCompatActivity) {
+        ActivityCompat.requestPermissions(activity, arrayOf("android.permission.GET_ACCOUNTS"), 100)
     }
 }
