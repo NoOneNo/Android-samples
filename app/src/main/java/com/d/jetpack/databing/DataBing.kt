ceff9f1b016c189211a7a4bff332ebe530330ec6
zhanghaoran <zhanghaoran@xiaomi.com>
2018-11-15 14:39:28 +0800

    update

---
+++ app/src/main/java/com/d/jetpack/databing/DataBing.kt
@@ -0,0 +1,15 @@
+package com.d.jetpack.databing
+
+import android.app.Activity
+//import android.databinding.DataBindingUtil
+import com.d.R
+
+data class User(val firstName: String, val lastName: String)
+
+class DataBingHelper {
+    fun create(activity: Activity) {
+//        val binding = DataBindingUtil.setContentView<DatabingBin>(activity, R.layout.databing)
+
+//        binding.set
+    }
+}
